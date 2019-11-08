c1b3849fb8decb0ae45ec077004aa1ebb8592d97
Dominic Hui <dominichui112@gmail.com>
2019-11-08 10:47:56 +0000

    registered routes in main.js and updated thanks.html

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ const port = 3000,
     contentTypes = require("./contentTypes"),
     utils = require("./utils");
 
+router.get("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+});
+
+router.get("/courses.html", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/courses.html", res);
+});
+
+router.get("/contact.html", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/contact.html", res);
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/thanks.html", res);
+});
+
+http.createServer(router.handle).listen(port);
+console.log(`the server is listening on port number ${port}`);
